633c9325a24a06b94943247ba98e5662056185d4
rajsinghkog <154885948+rajsinghkog@users.noreply.github.com>
2024-12-20 23:29:03 +0530

    Update popup.py

--- popup.py
+++ popup.py
@@ -2,10 +2,10 @@ from fastapi import FastAPI, HTTPException
 from transformers import pipeline
 from youtube_transcript_api import YouTubeTranscriptApi
 
-# FastAPI setup
+
 app = FastAPI()
 
-# Specify the device as -1 to force the model to run on CPU
+
 summarizer = pipeline('summarization', model='sshleifer/distilbart-cnn-12-6', device=-1)
 
 @app.get("/summary")
@@ -17,13 +17,13 @@ async def summary_api(url: str):
         summary = summarizer(transcript)
         return {"summary": summary[0]['summary_text']}
     except Exception as e:
-        # Print error to log
+       
         print(f"Error: {str(e)}")
         raise HTTPException(status_code=500, detail=str(e))
 
 def get_transcript(video_id: str) -> str:
     try:
-        # Get the transcript of the video
+       
         transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
         transcript = ' '.join([entry['text'] for entry in transcript_list])
         return transcript
