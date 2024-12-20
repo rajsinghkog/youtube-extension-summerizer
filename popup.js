f5cbca0cf35c48c663c21b8f5469083fae0b05e2
rajsinghkog <154885948+rajsinghkog@users.noreply.github.com>
2024-12-20 23:28:31 +0530

    Update popup.js
    improving

--- popup.js
+++ popup.js
@@ -1,11 +1,11 @@
 document.getElementById('getSummaryBtn').addEventListener('click', async function() {
     const summaryElement = document.getElementById('summary');
 
-    // Get the current tab's URL
+
     chrome.tabs.query({ active: true, currentWindow: true }, async function(tabs) {
         const url = tabs[0].url;
 
-        // Check if the URL is a valid YouTube URL
+       
         if (url && url.includes('youtube.com/watch?v=')) {
             try {
                 const response = await fetch(`http://localhost:8000/summary?url=${encodeURIComponent(url)}`);
